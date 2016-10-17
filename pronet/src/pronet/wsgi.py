3e17656df532e401794abbf95fb55a2da94486df
Ian Hunter <ian.hunter@sjsu.edu>
2016-10-17 13:30:29 -0700

    Update wsgi.py

--- pronet/src/pronet/wsgi.py
+++ pronet/src/pronet/wsgi.py
@@ -7,10 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 import os
-import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pronet.settings.production")
-
-sys.path.insert(0, '/opt/python/current/pronet/src/pronet/wsgi.py')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
