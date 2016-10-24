392a02503122d7cf4d3cb8d6df49801264a174e1
Jack Dubil <jackdubil@gmail.com>
2016-10-24 11:47:00 -0700

    Added premium user flag to profile model, display for it in show profile and a checkbox for it in sign up. Also some progress towards editing user experience, but still trying to figure that out.

--- pronet/src/profiles/forms.py
+++ pronet/src/profiles/forms.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
+from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field, Fieldset, MultiField
 from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 from django.contrib.auth import get_user_model
 from . import models
@@ -43,3 +43,37 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = models.Profile
         fields = ['picture', 'bio', 'skills', 'work_years', 'degree', 'resume',]
+
+
+class WorkExperienceForm(forms.ModelForm):
+    """
+    Form for entering individual work experiences.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(WorkExperienceForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            #Field('start_date'),
+            #Field('end_date'),
+            #Field('current'),
+            #Field('location'),
+            #Field('details'),
+            #Field('company'),
+            #Field('title'),
+            MultiField(
+                'Work Experience',
+                'start_date',
+                'end_date',
+                'current',
+                'location',
+                'details',
+                'company',
+                'title',
+            ),
+        )
+
+    class Meta:
+        model = models.WorkExperience
+        fields = ['start_date', 'end_date', 'current', 'location', 'details', 'company', 'title',]
