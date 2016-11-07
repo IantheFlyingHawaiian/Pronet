77a57de41cc22dd76f9fb13ce35dafacf6873485
HokuPop <hokupopco@gmail.com>
2016-11-07 11:10:37 -0800

    Search Implemented
    Conflicts:
    	pronet/src/profiles/forms.py
    	pronet/src/profiles/models.py
    	pronet/src/profiles/templates/profiles/show_profile.html

--- pronet/src/search/urls.py
+++ pronet/src/search/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import  url
+from django.conf.urls import url
 from . import views
 
 urlpatterns =[
-    url(r'^$', views.search, name='search'),
+    url(r'^SearchName$', views.Search.as_view(), name='search'),
+    url(r'^SearchYears$', views.SearchYears.as_view(), name='searchyears'),
 ]
