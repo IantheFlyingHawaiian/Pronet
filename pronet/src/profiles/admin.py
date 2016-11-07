8931a52a1119faeaaba9e72be95f3f0751029078
heisasianizer03 <pauldiaz@my.smccd.edu>
2016-11-07 11:45:55 -0800

    Bon jour! 11/7/16

--- pronet/src/profiles/admin.py
+++ pronet/src/profiles/admin.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 from authtools.admin import NamedUserAdmin
-from .models import Profile
+from .models import Profile, WorkExperience
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 
@@ -29,3 +29,4 @@ class NewUserAdmin(NamedUserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, NewUserAdmin)
+admin.site.register(WorkExperience)
--- pronet/src/search/views.py
+++ pronet/src/search/views.py
@@ -4,13 +4,78 @@ from django.template import loader
 from django.shortcuts import render
 from profiles import models
 
+class Search(generic.TemplateView):
+    def get(self, request):
+        template_name = "search/search_username.html"
+        users = models.Profile.objects.all()
+        return render(request, template_name, {'users': users})
 
-def search(request):
-    template_name = "search/search_it.html"
-    users = models.Profile.objects.all()
-    context = {'users': users}
-    return render(request, template_name, context)
-    '''
+class SearchYears(generic.TemplateView):
+    def get(self, request):
+        template_name = "search/search_yearsExp.html"
+        users = models.Profile.objects.all()
+        return render(request, template_name, {'users': users})
+
+
+'''
+    template_name = 'search/search_username.html'
+
+    def get(self, request, *args, **kwargs):
+        users = models.Profile.objects.all()
+        return render(request, self.template_name, {'user': users})
+
+
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'user': users})
+'''
+
+'''
+    template_name = "search/search_username.html"
+    http_method_names = ['get']
+
+    def get(self, request, context=None):
+        context = context or {}
+        context[0] = models.Profile.objects.all()
+
+        return super(Search, self).get(request, context=context)
+
+    def search(request):
+        template_name = "search/search_username.html"
+
+
+        return render(request, template_name, {'users': users})
+
+
+
+
+
+
+
+
+    url(r'^me$', views.ShowProfile.as_view(), name='show_self'),
+    template_name = "profiles/show_profile.html"
+    http_method_names = ['get']
+
+    def get(self, request, *args, **kwargs):
+        slug = self.kwargs.get('slug')
+        if slug:
+            profile = get_object_or_404(models.Profile, slug=slug)
+            user = profile.user
+        else:
+            user = self.request.user
+
+        if user == self.request.user:
+            kwargs["editable"] = True
+        kwargs["show_user"] = user
+        return super(ShowProfile, self).get(request, *args, **kwargs)
+
+class SearchYears():
+    def searchyears(request):
+        template_name = "search/search_yearsExp.html"
+        users = models.Profile.objects.all()
+
+        return render(request, template_name, {'users': users})
+
     class ShowProfile(LoginRequiredMixin, generic.TemplateView):
     template_name = "profiles/show_profile.html"
     http_method_names = ['get']
@@ -29,9 +94,9 @@ def search(request):
         return super(ShowProfile, self).get(request, *args, **kwargs)
 
     users = models.Profile.objects.all()
-    template = loader.get_template('search/search_it.html')
+    template = loader.get_template('search/search_username.html')
     context = { 'users':users }
 
     print(context)
-    return HttpResponse(template.render(context, request))
+
     '''
