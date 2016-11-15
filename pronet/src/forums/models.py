f550f824bac7291734ac43a2cf71f7e3f54c738f
jarechiga11 <j.arechiga11@gmail.com>
2016-11-14 18:53:32 -0800

    forum updates

--- pronet/src/forums/models.py
+++ pronet/src/forums/models.py
@@ -9,25 +9,26 @@ class Topic(models.Model):
 
 
 class Question(models.Model):
-    q_topic = models.ForeignKey(Topic, on_delete=models.PROTECT, default = None)
+    q_topic_id = models.ForeignKey(Topic, on_delete=models.PROTECT, default = None)
     question_text = models.CharField(max_length=200, default = "")
     pub_date = models.DateTimeField('date published')
+    #answers = models.ManyToManyField(Answer)
 
     def __str__(self):
         return self.question_text
 
-
-
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    a_question_topic_id = models.ForeignKey(Question, on_delete=models.PROTECT, default = None)
     answer_text = models.CharField(max_length=200)
 
     def __str__(self):
         return self.answer_text
 
+'''
 class Post(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     post_text = models.CharField(max_length = 500)
 
     def __str__(self):
         return self.post_text
+'''
