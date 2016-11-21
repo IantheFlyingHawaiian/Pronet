09012b993ab1635d5d1626730af359f1f6a03546
HokuPop <hokupopco@gmail.com>
2016-11-21 11:06:54 -0800

    Search Implemented
    Conflicts:
    	pronet/src/profiles/forms.py
    	pronet/src/profiles/models.py
    	pronet/src/profiles/templates/profiles/show_profile.html

--- pronet/src/search/urls.py
+++ pronet/src/search/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns =[
     url(r'^SearchName$', views.Search.as_view(), name='search'),
-    url(r'^SearchYears$', views.SearchYears.as_view(), name='searchyears'),
 ]
--- pronet/src/search/views.py
+++ pronet/src/search/views.py
@@ -6,97 +6,14 @@ from profiles import models
 
 class Search(generic.TemplateView):
     def get(self, request):
-        template_name = "search/search_username.html"
-        users = models.Profile.objects.all()
-        return render(request, template_name, {'users': users})
+        template_premium = "search/search_premium.html"
+        template_nonpremium = "search/search_nonpremium.html"
 
-class SearchYears(generic.TemplateView):
-    def get(self, request):
-        template_name = "search/search_yearsExp.html"
-        users = models.Profile.objects.all()
-        return render(request, template_name, {'users': users})
+        currentuser = self.request.user
+        queryset = models.Profile.objects.all()
+        exists = models.Profile.objects.filter(user=currentuser, premium_flag=True).exists()
 
-
-'''
-    template_name = 'search/search_username.html'
-
-    def get(self, request, *args, **kwargs):
-        users = models.Profile.objects.all()
-        return render(request, self.template_name, {'user': users})
-
-
-    def post(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'user': users})
-'''
-
-'''
-    template_name = "search/search_username.html"
-    http_method_names = ['get']
-
-    def get(self, request, context=None):
-        context = context or {}
-        context[0] = models.Profile.objects.all()
-
-        return super(Search, self).get(request, context=context)
-
-    def search(request):
-        template_name = "search/search_username.html"
-
-
-        return render(request, template_name, {'users': users})
-
-
-
-
-
-
-
-
-    url(r'^me$', views.ShowProfile.as_view(), name='show_self'),
-    template_name = "profiles/show_profile.html"
-    http_method_names = ['get']
-
-    def get(self, request, *args, **kwargs):
-        slug = self.kwargs.get('slug')
-        if slug:
-            profile = get_object_or_404(models.Profile, slug=slug)
-            user = profile.user
+        if exists:
+            return render(request, template_premium, {'users': queryset, 'cuurentuser':currentuser})
         else:
-            user = self.request.user
-
-        if user == self.request.user:
-            kwargs["editable"] = True
-        kwargs["show_user"] = user
-        return super(ShowProfile, self).get(request, *args, **kwargs)
-
-class SearchYears():
-    def searchyears(request):
-        template_name = "search/search_yearsExp.html"
-        users = models.Profile.objects.all()
-
-        return render(request, template_name, {'users': users})
-
-    class ShowProfile(LoginRequiredMixin, generic.TemplateView):
-    template_name = "profiles/show_profile.html"
-    http_method_names = ['get']
-
-    def get(self, request, *args, **kwargs):
-        slug = self.kwargs.get('slug')
-        if slug:
-            profile = get_object_or_404(models.Profile, slug=slug)
-            user = profile.user
-        else:
-            user = self.request.user
-
-        if user == self.request.user:
-            kwargs["editable"] = True
-        kwargs["show_user"] = user
-        return super(ShowProfile, self).get(request, *args, **kwargs)
-
-    users = models.Profile.objects.all()
-    template = loader.get_template('search/search_username.html')
-    context = { 'users':users }
-
-    print(context)
-
-    '''
+            return render(request, template_nonpremium, {'users': queryset, 'cuurentuser':currentuser})
