d801acfc009d5a4a313276fc99d99e690242b1fc
paulwilliamsdiaz <paul.diaz@sjsu.edu>
2016-11-29 08:12:00 -0800

    Searching completed

--- pronet/src/search/views.py
+++ pronet/src/search/views.py
@@ -17,62 +17,62 @@ class Search(generic.TemplateView):
        queryset_work = models.WorkExperience.objects.all()
        queryset_skill = models.SkillExperience.objects.all()
        queryset_education = models.University.objects.all()
-       queryset = models.Profile.objects.all()
+       queryset = None
 
        exists = models.Profile.objects.filter(user=currentuser, premium_flag=True).exists()
 
        #username search
        npquery = request.GET.get("npsearch")
-       pquery = request.GET.get("psearch")
        if npquery:
+           queryset = models.Profile.objects.all()
            queryset = queryset.filter(Q(user__name__icontains=npquery))
-       if pquery:
-           queryset = queryset.filter(Q(user__name__icontains=pquery))
 
        #SEARCH by EDUCATION
        pquery_degree = request.GET.get("degree")
-       if pquery_degree:
-           queryset = queryset.filter(Q(degree__icontains=pquery_degree))
+       if npquery == None:
+           if pquery_degree:
+               queryset = models.Profile.objects.all()
+               queryset = queryset.filter(Q(degree__icontains=pquery_degree))
 
        #SEARCH by SKILLS
        pquery_skill = request.GET.get("skill")
        pquery_years = request.GET.get("yskill")
-       if pquery_years:
-           pquery_years = Decimal(pquery_years)
+       if npquery == None and pquery_degree == None:
 
-       if pquery_skill != None and pquery_years != 0:
-           queryset_skill = queryset_skill.filter(Q(skill_name__icontains=pquery_skill) &
-                                        Q(skill_years__gte=pquery_years))
+           if queryset_skill != None or pquery_years != 0:
+               if pquery_years:
+                   pquery_years = Decimal(pquery_years)
 
-       if pquery_skill != None and pquery_years == 0:
-           queryset_skill = queryset_skill.filter(Q(skill_name__icontains=pquery_skill))
+               if pquery_skill != None and pquery_years != 0:
+                   queryset_skill = queryset_skill.filter(Q(skill_name__icontains=pquery_skill) &
+                                                Q(skill_years__gte=pquery_years))
 
-       for user in queryset_skill:
-           queryset = queryset.filter(user__profile=user.profile)
+               if pquery_skill != None and pquery_years == 0:
+                   queryset_skill = queryset_skill.filter(Q(skill_name__icontains=pquery_skill))
+
+               queryset = models.Profile.objects.all()
+
+               for user in queryset_skill:
+                   queryset = queryset.filter(user__profile=user.profile)
 
 
        #SEARCH by WORK EXPERIENCE
        pquery_yearWorkExp = request.GET.get("ywork")
        pquery_company = request.GET.get("cmpy")
-       if pquery_yearWorkExp:
-            pquery_yearWorkExp = Decimal(pquery_yearWorkExp)
+       if npquery == None and pquery_degree == None and pquery_skill == None and pquery_years == None:
+           if pquery_yearWorkExp:
+                pquery_yearWorkExp = Decimal(pquery_yearWorkExp)
 
-       if pquery_company != None and pquery_yearWorkExp != 0:
-           queryset_work = queryset_work.filter(Q(company__icontains=pquery_company))#include pquery_yearWorkExp
+           if pquery_company != None and pquery_yearWorkExp != 0:
+               queryset_work = queryset_work.filter(Q(company__icontains=pquery_company))#include pquery_yearWorkExp
 
-       if pquery_company != None and pquery_yearWorkExp == 0:
-           queryset_work = queryset_work.filter(Q(company__icontains=pquery_company))
+           if pquery_company != None and pquery_yearWorkExp == 0:
+               queryset_work = queryset_work.filter(Q(company__icontains=pquery_company))
 
-       for user in queryset_work:
-           queryset = queryset.filter(user__profile=user.profile)
+           queryset = models.Profile.objects.all()
 
-
-       if pquery_skill == None:
-           queryset = None
-
-       if pquery_yearWorkExp == None:
-           queryset = None
-
+           for user in queryset_work:
+               queryset = queryset.filter(user__profile=user.profile)
 
        if exists:
            return render(request, template_premium, {'users': queryset, 'currentuser': currentuser})
